8f6f95ad1c32eba7ee71e7e563245bc81f455773
dacanizares <dacanizares@outlook.com>
2023-05-18 23:46:35 -0500

    Added ult commons
    Co-Authored-By: Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>

---
+++ ult_common.h
@@ -0,0 +1,47 @@
+#pragma once
+#include "ult_globals.h"
+
+struct buffer
+{
+    uint8* Data;
+    int32 Size;
+};
+typedef buffer string;
+
+#define BundleStringC(S) MakeString((uint8*)S, strlen(S))
+#define BundleString(S) MakeString(S, strlen((char*)S))
+inline function string
+MakeString(uint8* Address, int32 Size)
+{
+    string NewString;
+    NewString.Data = Address;
+    NewString.Size = Size;
+    return(NewString);
+}
+
+inline internal uint32
+GenerateHash(void* MemoryBlock, uint32 SizeInBytes)
+{
+    uint8* Ptr = (uint8*)MemoryBlock;
+    uint32 Result = 0;
+    for(int i = 0; i < SizeInBytes; ++i)
+	{
+		Result = Ptr[i] + (Result * 31);
+	}
+    return(Result);
+}
+
+inline function bool
+IsLittleEndian()
+{
+    int32 One = 1;
+    return (*((uint8*)&One) == 1);
+}
+
+#define ChangeEndianness16(n) n<<8 | n >>8;
+#define ChangeEndianness32(n) ((n>>24)&0xff) | ((n<<8)&0xff0000) | ((n>>8)&0xff00) | ((n<<24)&0xff000000)
+
+#define UNLIKELY(x) __builtin_expect((bool)x, 0)
+#define Max(a,b) a < b ? b : a
+#define Mod(a,b) a >= b ? a % b : a
+#define UnlikelyMod(a,b) UNLIKELY((a >= b)) ? a % b : a
