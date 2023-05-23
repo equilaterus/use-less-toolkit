88d50418d8284fdcc16f574a90f50e5cbcc87cb4
dacanizares <dacanizares@outlook.com>
2023-05-22 20:46:01 -0500

    Added dockspace (WIP)
    Pending clean-up

--- main.cpp
+++ main.cpp
@@ -197,6 +197,99 @@ int main(int, char**)
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
+        // Dockspace
+        static ImGuiID dockspace_id;
+        static bool opt_fullscreen = true;
+        static bool opt_padding = false;
+        static bool open_dockspace = true;
+        static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_None;
+
+        // We are using the ImGuiWindowFlags_NoDocking flag to make the parent window not dockable into,
+        // because it would be confusing to have two docking targets within each others.
+        ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
+        if (opt_fullscreen)
+        {
+            const ImGuiViewport* viewport = ImGui::GetMainViewport();
+            ImGui::SetNextWindowPos(viewport->WorkPos);
+            ImGui::SetNextWindowSize(viewport->WorkSize);
+            ImGui::SetNextWindowViewport(viewport->ID);
+            ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+            ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+            window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
+            window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
+        }
+        else
+        {
+            dockspace_flags &= ~ImGuiDockNodeFlags_PassthruCentralNode;
+        }
+
+        // When using ImGuiDockNodeFlags_PassthruCentralNode, DockSpace() will render our background
+        // and handle the pass-thru hole, so we ask Begin() to not render a background.
+        if (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
+            window_flags |= ImGuiWindowFlags_NoBackground;
+
+        // Important: note that we proceed even if Begin() returns false (aka window is collapsed).
+        // This is because we want to keep our DockSpace() active. If a DockSpace() is inactive,
+        // all active windows docked into it will lose their parent and become undocked.
+        // We cannot preserve the docking relationship between an active window and an inactive docking, otherwise
+        // any change of dockspace/settings would lead to windows being stuck in limbo and never being visible.
+        if (!opt_padding)
+            ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+        ImGui::Begin("DockSpace Demo", &open_dockspace, window_flags);
+        if (!opt_padding)
+            ImGui::PopStyleVar();
+
+        if (opt_fullscreen)
+            ImGui::PopStyleVar(2);
+
+        // Submit the DockSpace
+        ImGuiIO& io = ImGui::GetIO();
+        if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
+        {
+            dockspace_id = ImGui::GetID("MyDockSpace");
+            ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
+        }
+        else
+        {
+            ShowDockingDisabledMessage();
+        }
+
+        if (ImGui::BeginMenuBar())
+        {
+            if (ImGui::BeginMenu("Options"))
+            {
+                // Disabling fullscreen would allow the window to be moved to the front of other windows,
+                // which we can't undo at the moment without finer window depth/z control.
+                ImGui::MenuItem("Fullscreen", NULL, &opt_fullscreen);
+                ImGui::MenuItem("Padding", NULL, &opt_padding);
+                ImGui::Separator();
+
+                if (ImGui::MenuItem("Flag: NoSplit",                "", (dockspace_flags & ImGuiDockNodeFlags_NoSplit) != 0))                 { dockspace_flags ^= ImGuiDockNodeFlags_NoSplit; }
+                if (ImGui::MenuItem("Flag: NoResize",               "", (dockspace_flags & ImGuiDockNodeFlags_NoResize) != 0))                { dockspace_flags ^= ImGuiDockNodeFlags_NoResize; }
+                if (ImGui::MenuItem("Flag: NoDockingInCentralNode", "", (dockspace_flags & ImGuiDockNodeFlags_NoDockingInCentralNode) != 0))  { dockspace_flags ^= ImGuiDockNodeFlags_NoDockingInCentralNode; }
+                if (ImGui::MenuItem("Flag: AutoHideTabBar",         "", (dockspace_flags & ImGuiDockNodeFlags_AutoHideTabBar) != 0))          { dockspace_flags ^= ImGuiDockNodeFlags_AutoHideTabBar; }
+                if (ImGui::MenuItem("Flag: PassthruCentralNode",    "", (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode) != 0, opt_fullscreen)) { dockspace_flags ^= ImGuiDockNodeFlags_PassthruCentralNode; }
+                ImGui::Separator();
+
+                if (ImGui::MenuItem("Close", NULL, false, open_dockspace))
+                    open_dockspace = 0;
+                ImGui::EndMenu();
+            }
+            HelpMarker(
+                "When docking is enabled, you can ALWAYS dock MOST window into another! Try it now!" "\n"
+                "- Drag from window title bar or their tab to dock/undock." "\n"
+                "- Drag from window menu button (upper-left button) to undock an entire node (all windows)." "\n"
+                "- Hold SHIFT to disable docking (if io.ConfigDockingWithShift == false, default)" "\n"
+                "- Hold SHIFT to enable docking (if io.ConfigDockingWithShift == true)" "\n"
+                "This demo app has nothing to do with enabling docking!" "\n\n"
+                "This demo app only demonstrate the use of ImGui::DockSpace() which allows you to manually create a docking node _within_ another window." "\n\n"
+                "Read comments in ShowExampleAppDockSpace() for more details.");
+
+            ImGui::EndMenuBar();
+        }
+
+        ImGui::End();
+
         // Build UI
         if (State.Settings.ShowDemoWindow)
            ImGui::ShowDemoWindow(&State.Settings.ShowDemoWindow);
@@ -220,6 +313,39 @@ int main(int, char**)
             ImGui::Begin("Applications", &State.Settings.ShowApplicationsWindow);            
             DisplayDirectory(State.Applications, 0);
             ImGui::End();
+        }
+
+        
+
+        static bool f = 0;
+        if (!f){
+            ImGui::DockBuilderRemoveNode(dockspace_id); // clear any previous layout
+		    ImGui::DockBuilderAddNode(dockspace_id, ImGuiDockNodeFlags_None);
+            f = 1;
+            const ImVec2 dockspace_size = ImGui::GetContentRegionAvail();
+            ImGui::DockBuilderSetNodeSize(dockspace_id, dockspace_size);
+        }
+        
+        ImGuiDockNode* dock_node = ImGui::DockBuilderGetNode(dockspace_id);
+        if (dock_node && !dock_node->IsSplitNode())
+        {
+            ImGuiID nodeLeft;
+            ImGuiID nodeRight;
+            ImGui::DockBuilderSplitNode(dockspace_id, ImGuiDir_Right, 0.4f, &nodeRight, &nodeLeft);
+
+            ImGuiID nodeLeftUp;
+            ImGuiID nodeLeftDown;
+            ImGui::DockBuilderSplitNode(nodeLeft, ImGuiDir_Up, 0.5f, &nodeLeftUp, &nodeLeftDown);
+
+            ImGuiID nodeRightUp;
+            ImGuiID nodeRightDown;
+            ImGui::DockBuilderSplitNode(nodeRight, ImGuiDir_Up, 0.5f, &nodeRightUp, &nodeRightDown);
+
+            ImGui::DockBuilderDockWindow("Applications", nodeLeftUp);
+            ImGui::DockBuilderDockWindow("Scripts", nodeLeftDown);
+
+            ImGui::DockBuilderDockWindow("Settings", nodeRightUp);
+            ImGui::DockBuilderDockWindow("Dear ImGui Demo", nodeRightDown);
         }
 
         // Rendering
