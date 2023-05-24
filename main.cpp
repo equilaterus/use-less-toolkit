e00c158fce4d5e1c75e31ff24e4f5e4f84fd4be8
dacanizares <dacanizares@outlook.com>
2023-05-24 11:00:18 -0500

    Added icon support

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@
 #include <cstring>
 #include <dirent.h> 
 #include <iterator>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb/stb_image.h"
+
 
 #include "imgui/imgui.cpp"
 #include "imgui/imgui.h"
@@ -166,6 +169,11 @@ int main(int, char**)
         return 1;
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1); // Enable vsync
+
+    GLFWimage images[1]; 
+    images[0].pixels = stbi_load("assets/icon.png", &images[0].width, &images[0].height, 0, 4); //rgba channels 
+    glfwSetWindowIcon(window, 1, images); 
+    stbi_image_free(images[0].pixels);
     
     // Setup Dear ImGui context
     IMGUI_CHECKVERSION();
